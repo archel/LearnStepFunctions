05c0016a44083a93d0cc89e86bee224ee19e3ee6
Daniel Jordi Pérez <danieljordi@bab-soft.com>
2017-08-05 12:09:34 +0200

    Some name refactor and implementation of step 5

--- DefenseMultiplierLambda/handler.js
+++ DefenseMultiplierLambda/handler.js
@@ -4,11 +4,11 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-module.exports.attackMultiplier = (event, context, callback) => {
+module.exports.defenseMultiplier = (event, context, callback) => {
   let multiplier = getRandomIntInclusive(0, 2),
       turn  = event;
     
-  turn.Attack.Strength = multiplier * turn.Attack.Strength;
+  turn.Defense.Strength = multiplier * turn.Defense.Strength;
 
   callback(null, turn);
 };
